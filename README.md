1502d3970805809cfa703c3e7ea06b39efcb76be
Vanshika-Dargan <vanshikadargan.vd@gmail.com>
2024-10-24 21:09:13 +0530

    added function for decoding bencoded values only for strings as of now

---
+++ README.md
@@ -0,0 +1,2 @@
+Torrent is a peer to peer file sharing protocol used for distributing large amounts of data. Torrent is a torrent client capable of downloading publicly available file using the bit torrent protocol.
+
---
+++ src/index.js
@@ -0,0 +1,35 @@
+const process = require('process');
+
+
+function decodeBencode(bencodedValue){
+
+    if(!isNaN(bencodedValue[0])){
+    const firstColonIndex = bencodedValue.indexOf(":");
+    if(firstColonIndex === -1){
+    throw new Error('Invalid encoded value');
+    }
+    return bencodedValue.substr(firstColonIndex+1);
+}
+else {
+    throw new Error('Not a valid string')
+}
+}
+
+
+
+
+
+function main(){
+const command= process.argv[2];
+
+if(command === 'decode'){
+const bencodedValue = process.argv[3];
+console.log(JSON.stringify(decodeBencode(bencodedValue)));
+}
+else{
+    throw new Error(`Unknown command ${command}`)
+}
+
+}
+
+main();
